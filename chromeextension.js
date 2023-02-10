53498b4dbc64fd6607245b977aeb67b763731dfb
alxspiker <alxtheprogrammer@gmail.com>
2023-02-10 14:49:45 -0700

    Update chromeextension.js

--- chromeextension.js
+++ chromeextension.js
@@ -2,9 +2,9 @@ var buttons = document.getElementsByClassName('button-bar')[0].getElementsByTagN
 
 for( var i=0; i< buttons.length; i++ )
 {
-	if(buttons[i].innerHTML.trim()=="Edit Order"){
+	if(buttons[i].innerText.trim()=="Edit Order"){
 		buttons[i].href = window.location.href+"/edit";
-	}else if(buttons[i].innerHTML.trim()=="Export CSV"){
+	}else if(buttons[i].innerText.trim()=="Export CSV"){
 		buttons[i].href = window.location.href+"/export";
 	}
 	var childDiv = buttons[i];
