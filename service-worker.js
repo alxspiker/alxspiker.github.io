7584bfb331cb8879ffb4ae8c24646905d94a137d
alxspiker <alxtheprogrammer@gmail.com>
2024-02-27 08:18:15 -0700

    service worker

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/styles.css',
+  '/script.js',
+  // Add other assets you want to cache
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
