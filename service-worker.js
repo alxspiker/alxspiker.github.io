4083acfeb261b2c98ea293b5e049667bbaf4edae
alxspiker <alxtheprogrammer@gmail.com>
2024-02-27 08:46:35 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
 const CACHE_NAME = 'qubixia_v1.0.0';
 const urlsToCache = [
-  '/',
+  '/index.html',
+  '/qubixia.v1.js',
   // Add other assets you want to cache
 ];
 
@@ -11,37 +12,9 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          // Check if it's an outdated cache
-          if (cacheName !== CACHE_NAME) {
-            // Delete the old cache
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
 self.addEventListener('fetch', event => {
-  // Check if the request is for the specific JS file
-  if (event.request.url.endsWith('qubixia.v1.js')) {
-    event.respondWith(
-      // Prioritize network to ensure you get the updated version.
-      fetch(event.request).catch(() => {
-        // Fallback to cache if network fails.
-        return caches.match(event.request);
-      }) 
-    );
-  } else {
-    // For other requests, continue with the original logic
-    event.respondWith(
-      caches.match(event.request)
-        .then(response => response || fetch(event.request))
-    );
-  }
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
