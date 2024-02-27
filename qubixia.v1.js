c1809c8cc6cc7a3c620efd0893b5a95d869e569b
alxspiker <alxtheprogrammer@gmail.com>
2024-02-27 11:04:09 -0700

    Update qubixia.v1.js

--- qubixia.v1.js
+++ qubixia.v1.js
@@ -4,15 +4,28 @@ function update_page_content(url){
     document.getElementById("page-content").src = MainUrl+url;
 }
 
+function pi_login(event){
+    // Get the data from the message
+    var data = event.data;
+
+    Pi.authenticate(['payments','username'], onIncompletePaymentFound).then(function(auth) {
+        //Pi.authenticate(['payments'], onIncompletePaymentFound).then(function(auth) {
+        Pi.LoggedInUser = auth;
+        // Send a message back to the iframe
+        event.source.postMessage(auth, event.origin);
+        //user_login_complete(auth);
+    }).catch(function(error) {
+        Pi.LoggedInUser = null;
+        if (confirm('Failed to authenticate, please refresh the page to try again:'+error)) {
+            window.location.reload();
+        }
+    });
+  }
+
 window.addEventListener("message", function (event) {
     // Check if the message is from the iframe
     if (event.origin == "https://qubixia.bubbleapps.io") {
-        // Get the data from the message
-        var data = event.data;
-        // Do something with the data
-        console.log("Parent:"+data);
-        // Send a message back to the iframe
-        event.source.postMessage("Got it!", event.origin);
+        pi_login(event);
     }
 });
 
