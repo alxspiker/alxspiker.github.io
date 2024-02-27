f5313a359d6fc209303086c1870696f843a3e904
alxspiker <alxtheprogrammer@gmail.com>
2024-02-27 08:47:12 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'qubixia_v1.0.0';
+const CACHE_NAME = 'qubixia_v1.0.1';
 const urlsToCache = [
   '/index.html',
   '/qubixia.v1.js',
+  '/validation-key.txt',
   // Add other assets you want to cache
 ];
 
