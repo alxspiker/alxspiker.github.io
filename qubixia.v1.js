bba09d515c4d9c8a0b9b70966cc8e46ded784976
alxspiker <alxtheprogrammer@gmail.com>
2024-03-01 08:33:04 -0700

    js

--- qubixia.v1.js
+++ qubixia.v1.js
@@ -11,25 +11,6 @@ function onIncompletePaymentFound(payment) {
     alert('incomplete:'+payment);
 };
 
-function pi_login(event){
-    // Get the data from the message
-    var data = event.data;
-
-    Pi.authenticate(['username','payments','wallet_address'], onIncompletePaymentFound).then(function(auth) {
-    //Pi.authenticate(['payments','username'], onIncompletePaymentFound).then(function(auth) {
-        //Pi.authenticate(['payments'], onIncompletePaymentFound).then(function(auth) {
-        Pi.LoggedInUser = auth;
-        // Send a message back to the iframe
-        update_page_content("pinet-authenticate?data="+JSON.stringify(auth), false);
-        //user_login_complete(auth);
-    }).catch(function(error) {
-        Pi.LoggedInUser = null;
-        if (confirm('Failed to authenticate, please refresh the page to try again:'+error)) {
-            window.location.reload();
-        }
-    });
-  }
-
 window.addEventListener("message", function (event) {
     // Check if the message is from the iframe
     if (event.origin == "https://qubixia.bubbleapps.io") {
@@ -41,7 +22,32 @@ window.addEventListener("message", function (event) {
             // Execute the appropriate function
             switch (command) {
                 case "pinet_authenticate":
+                    function pi_login(event){
+                        // Get the data from the message
+                        var data = event.data;
+                    
+                        Pi.authenticate(['username','payments','wallet_address'], onIncompletePaymentFound).then(function(auth) {
+                        //Pi.authenticate(['payments','username'], onIncompletePaymentFound).then(function(auth) {
+                            //Pi.authenticate(['payments'], onIncompletePaymentFound).then(function(auth) {
+                            Pi.LoggedInUser = auth;
+                            // Send a message back to the iframe
+                            update_page_content("pinet-authenticate?data="+JSON.stringify(auth), false);
+                            //user_login_complete(auth);
+                        }).catch(function(error) {
+                            Pi.LoggedInUser = null;
+                            if (confirm('Failed to authenticate, please refresh the page to try again:'+error)) {
+                                window.location.reload();
+                            }
+                        });
+                    }
                     pi_login(event);
+                    break;
+                case "pinet_deposit":
+                    function pi_deposit(event) {
+                        var data = event.data;
+                        alert(toString(data));
+                    }
+                    pi_deposit(event);
                     break;
                 default:
                     break;
